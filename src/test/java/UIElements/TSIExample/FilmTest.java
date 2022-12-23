74f81b2adca323b5224425ba09d9370f5dafa3a7
sarpulu <s.ulutuncel@gmail.com>
2022-12-23 09:51:36 +0000

    JUnit testing started film tests

---
+++ src/test/java/UIElements/TSIExample/FilmTest.java
@@ -0,0 +1,22 @@
+package UIElements.TSIExample;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilmTest {
+
+    @Test
+    public void testEmptyConstructor(){
+        Film film = new Film();
+        Assertions.assertNotNull(film);
+    }
+
+//    @Test
+//    public void testGetFilmID(){
+//        Film film = new Film();
+//        Assertions.assertNotNull(film.getFilmID());
+//    }
+//
+//    @Test
+//    public void set
+}
